f8c1825c5b20732c041ac9cf647ed6e9035917c7
Derek Knight (mac) <Derek@GordonKnight.co.uk>
2013-07-28 12:20:29 +1200

    Stripped old application.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Elements::Application.config.secret_token = 'aebc95077aafb70f4b535f84b0c74e5ffc94a2c1edd1d2f9f10ba0a5aa7bfeb4b575f8ef1aeb32ffeb696bece5a8db4931eddfc7b2094e05e9b4946a1ba80242'
