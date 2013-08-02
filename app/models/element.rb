0dc0f2435edabc2af3827ece881c41073912ef02
Derek Knight (Mac) <Derek@GordonKnight.co.uk>
2013-08-02 17:52:24 +1200

    Oops wrong comment characters

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,9 +1,9 @@
 class Element < ActiveRecord::Base
 
-  // Sort the records on atomic number
+  # Sort the records on atomic number
   default_scope order('atomic_number ASC')
 
-  // Designate a primary key
+  # Designate a primary key
   self.primary_key = :id
 
 end
