87842296b73e464ab7d7a54e9a55cf3dda2c54f2
Derek Knight (Mac) <Derek@GordonKnight.co.uk>
2013-08-22 19:06:13 +1200

    Now works, can deploy locally and to other web server. Key was using bash, not zsh. There must be something wring with my zsh setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,14 +14,10 @@ set :keep_releases, 1
 set :application, "elements"
 set :repository,  "git@github.com:DerekK19/RailsElements.git"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
 
-# if you want to clean up old releases on each deploy uncomment this:
+# clean up old releases on each deploy
 after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
   task :start do ; end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-role :web, "localhost"							# Your HTTP server, Apache/etc
-role :app, "localhost"							# This may be the same as your `Web` server
+role :web, "russet"							# Your HTTP server, Apache/etc
+role :app, "russet"							# This may be the same as your `Web` server
 #role :db,  "mysqlserver", :primary => true		# This is where Rails migrations will run
 #role :db,  "mysqlserver"
 
